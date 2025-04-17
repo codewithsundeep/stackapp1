e09ac61ccadca957ca8e1f39f026a4418ebbc80e
Sundeep Sharma <77357224+codewithsundeep@users.noreply.github.com>
2025-04-18 01:39:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.node = {
+      fs: 'empty',
+      path: 'empty',
+      stream: 'empty',
+    };
+    return config;
+  },
   output: 'standalone',
   experimental: {
     serverActions: true,
